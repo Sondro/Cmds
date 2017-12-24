5e4455fd0243f67ba14987fe5301cf0d0da87820
Sondro <memoiran@hotmail.com>
2017-12-24 14:02:25 -0800

    Initial commit

---
+++ Windows/bats/__mkdir_my-app_FULL.bat
@@ -0,0 +1,11 @@
+mkdir my-app
+cd my-app/
+echo.>NUL 2>server.js 
+echo.>NUL 2>app.js 
+echo.>NUL 2>docs.md 
+echo.>NUL 2>readme.md
+.gitignore 
+echo.>NUL 2>routes.test.js 
+echo.>NUL 2>models.test.js
+npm init --yes
+@pause
