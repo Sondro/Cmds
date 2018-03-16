a41a5d97d0f1c757d5e69b889288c65a1ec4ce1d
Sondro <memoiran@hotmail.com>
2018-03-16 15:50:33 -0700

    commit

--- Bash/sh/__[Python]_conda_install_or_pip - Copy.sh
+++ Bash/sh/__[Python]_conda_install_or_pip - Copy.sh
@@ -1,1 +1,3 @@
 $while read requirement; $do W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes $requirement || W:/CODE/Python/Python36-32/Scripts/pip.exe install $requirement; $done < requirements.txt
+read 
+$SHELL
