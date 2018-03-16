48c29859d91295b363e5f5ec70106a8b63575e2d
Sondro <memoiran@hotmail.com>
2018-03-16 16:16:19 -0700

    commit

---
+++ Windows/bats/__[Python_Django]_install_django_lib_pips--yes.bat
@@ -0,0 +1,33 @@
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes django==2.0.2
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes django-allauth==0.35.0
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes djangorestframework==3.7.7
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes graphene-django>=2.0.dev
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes django-filter==2.0.0.dev1
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes django_extensions==2.0.6
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes psycopg2-binary==2.7.4
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes gunicorn==19.7.1
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes python-decouple==3.1
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes whitenoise==3.3.1
+
+W:/CODE/Python/Anaconda/Anaconda3/Scripts/conda.exe install --yes pip
+W:/CODE/Python/Python36-32/Scripts/pip.exe install --yes dj-database-url==0.5.0
+@pause
