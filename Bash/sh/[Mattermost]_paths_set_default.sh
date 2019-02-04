962dc83e4dff7962a38bf10938e7a1d77126a163
Sondro <memoiran@hotmail.com>
2019-02-04 13:32:58 -0800

    commit

---
+++ Bash/sh/[Mattermost]_paths_set_default.sh
@@ -0,0 +1,7 @@
+export PATH="/c/Program Files/go/bin":$PATH
+export PATH="/c/Program Files/nodejs":$PATH
+export PATH="/c/Program Files/Git/bin":$PATH
+export GOROOT="c:\\Program Files\\go"
+export GOPATH="c:\\User\\{user-name}\\go"
+export PATH="/c/Program Files/Docker Toolbox":$PATH # change the path accordingly if you are using Docker for Windows
+eval $(docker-machine env default) # skip this line if you are using Docker for Windows
