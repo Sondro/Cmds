ba74409d3dc3bd0707652c9e7b9bcf4042456198
Sondro <memoiran@hotmail.com>
2021-08-02 18:36:41 -0700

    +

--- Windows/bats/__[sys]_chocolatey_install.bat
+++ Windows/bats/__[sys]_chocolatey_install.bat
@@ -1,3 +1,4 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& { Start-Process PowerShell -ArgumentList 'Set-ExecutionPolicy Restricted -Force -Scope Process; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(''https://chocolatey.org/install.ps1''))' -Verb RunAs }"
+
 
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit)
