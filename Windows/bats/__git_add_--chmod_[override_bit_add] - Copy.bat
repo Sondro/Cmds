b8d88660b9f19d0b91135ae4f2a1a8890c110a42
Sondro <memoiran@hotmail.com>
2022-08-31 11:06:18 -0700

    Rename __git_add_--chmod_[override_bit_add] - Copy.bat to __git_add_--chmod_[override_bit_add].bat

--- Windows/bats/__git_add_--chmod_[override_bit_add] - Copy.bat
+++
@@ -1,3 +0,0 @@
-git add --chmod=(+|-)x
-
-if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit) 
---
+++ Windows/bats/__git_add_--chmod_[override_bit_add].bat
@@ -0,0 +1,3 @@
+git add --chmod=(+|-)x
+
+if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit) 
