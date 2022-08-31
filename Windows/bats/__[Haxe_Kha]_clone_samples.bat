6060f73d76ffcc9f861e549549d2dce43e0a1284
Sondro <memoiran@hotmail.com>
2022-08-31 11:28:08 -0700

    Update __[Haxe_Kha]_clone_samples.bat

--- Windows/bats/__[Haxe_Kha]_clone_samples.bat
+++ Windows/bats/__[Haxe_Kha]_clone_samples.bat
@@ -1,3 +1,5 @@
-git clone --recursive https://github.com/Kha/Empty.git
+git clone https://github.com/Kha-Samples/Empty.git
+git submodule update --init
+Kha/get_dlc
 
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit)
